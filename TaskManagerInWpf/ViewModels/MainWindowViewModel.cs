2aaf6060edc59661667cff74765e01142cb4a822
Mahs_kz07 <Mahs_kz07@STHQ011A-16>
2021-11-01 17:48:15 +0400

    Add project files.

---
+++ TaskManagerInWpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagerInWpf.RelayCommands;
+
+namespace TaskManagerInWpf.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public RelayCommand CreateBtnRelayCommand { get; set; }
+        public ObservableCollection<Process> Processes { get; set; }
+        public MainWindowViewModel(MainWindow mainWindow)
+        {
+            Processes= new ObservableCollection<Process>(Process.GetProcesses());
+            
+            CreateBtnRelayCommand = new RelayCommand((sender) =>
+            {
+
+
+            });
+        }
+    } 
+    
+}
