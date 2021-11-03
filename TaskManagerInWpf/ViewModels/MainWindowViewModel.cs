cfb457eea0309c6f173e824c2037325241c14469
Mahs_kz07@itstep.edu.az <Mahs_kz07@itstep.edu.az>
2021-11-03 14:48:08 +0400

    /

--- TaskManagerInWpf/ViewModels/MainWindowViewModel.cs
+++ TaskManagerInWpf/ViewModels/MainWindowViewModel.cs
@@ -12,14 +12,34 @@ namespace TaskManagerInWpf.ViewModels
     public class MainWindowViewModel : BaseViewModel
     {
         public RelayCommand CreateBtnRelayCommand { get; set; }
+        public RelayCommand SelectedProcessCommand { get; set; }
+        public RelayCommand KillBtnClickCommand { get; set; }
         public ObservableCollection<Process> Processes { get; set; }
+        private Process selectedprocess;
+
+        public Process SelectedProcess
+        {
+            get { return selectedprocess; }
+            set { selectedprocess = value; }
+        }
+
         public MainWindowViewModel(MainWindow mainWindow)
         {
-            Processes= new ObservableCollection<Process>(Process.GetProcesses());
-            
+            Processes = new ObservableCollection<Process>(Process.GetProcesses());
+
+
             CreateBtnRelayCommand = new RelayCommand((sender) =>
             {
+                Process.Start(mainWindow.ProcessTextBox.Text);
 
+            });
+            
+            KillBtnClickCommand = new RelayCommand((sender) =>
+            {
+                if (SelectedProcess != null)
+                {
+                    SelectedProcess.Kill();
+                }
 
             });
         }
